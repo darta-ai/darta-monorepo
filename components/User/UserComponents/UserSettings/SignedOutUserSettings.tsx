ca93b498e45d297fabd4b4b3a8e39f167debe967
TJWetmore <thomas.wetmore@nate.tech>
2023-04-14 10:50:21 -0400

    tempalte for user sign in

---
+++ components/User/UserComponents/UserSettings/SignedOutUserSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+
+export function UserSettingsSignedOut() {
+  return (
+    <View>
+      <Text>Sign in will go here</Text>
+    </View>
+  );
+}
