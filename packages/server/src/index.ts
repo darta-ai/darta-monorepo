1c711104d37ea7eff6099d5907b2a6a40e46a724
TJWetmore <thomas.wetmore@nate.tech>
2023-04-22 16:06:41 -0400

    final commit of the day

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,10 +6,29 @@ const app = express();
 
 // Routes for User
 // Get user by id => return user object
+// get the data that they are going to view for Darta that day
+// get the data that is their saved work
+// get the data that is their inquired work
 
-app.get('/', async (req, res) => {
-  res.json({hello: 'world'});
+app.get('/user', async (req, res) => {
+  res.json({
+    profilePicture:
+      'https://www.shutterstock.com/image-photo/closeup-photo-amazing-short-hairdo-260nw-1617540484.jpg',
+    userName: 'user name 10000',
+    legalName: 'firstName lastName',
+    email: 'email@gmail.com',
+    phone: '(123) 123-4567',
+  });
 });
+
+app.post('/user', async (req, res) => {
+    res.json({
+        profilePicture:
+    )}
+)
+
+// Routes for interactions
+// create edge between user and artwork
 
 // Start the server
 const PORT = process.env.PORT || 1160;
