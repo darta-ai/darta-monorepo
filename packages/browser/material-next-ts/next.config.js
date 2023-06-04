709531c09c8fe3babd2ae0aaa2fa7168ed3ddb59
TJWetmore <thomas.wetmore@nate.tech>
2023-06-04 19:12:19 -0400

    wip

--- packages/browser/material-next-ts/next.config.js
+++ packages/browser/material-next-ts/next.config.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 const withVideos = require('next-videos');
 module.exports = {
-  reactStrictMode: true,
   env: {
     REACT_APP_FIREBASE_API_KEY: process.env.REACT_APP_FIREBASE_API_KEY,
     REACT_APP_FIREBASE_AUTH_DOMAIN: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -19,17 +18,16 @@ module.exports = {
     SUPERBASE_API_KEY: process.env.SUPERBASE_API_KEY,
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    loader: 'imgix',
+    path: 'https://s3.amazonaws.com',
+    domains: [
+      'lh3.googleusercontent.com',
+      's3.amazonaws.com',
+      'http://s3.amazonaws',
+    ],
     unoptimized: true,
+    reactStrictMode: true,
   },
 };
 
 module.exports = withVideos();
-
-// REACT_APP_FIREBASE_API_KEY : "AIzaSyAjZz2ggOnHZJ-g0_8Nuz5bJFSVh756YB4",
-// REACT_APP_FIREBASE_AUTH_DOMAIN:  "darta-desktop.firebaseapp.com",
-// REACT_APP_PROJECT_ID : "darta-desktop",
-// REACT_APP_FIREBASE_STORAGE_BUCKET : "darta-desktop.appspot.com",
-// REACT_APP_FIREBASE_SENDER_ID : "682927959309",
-// REACT_APP_FIREBASE_APP_ID : "1:682927959309:web:0b421dab61449b97041444",
-// REACT_APP_FIREBASE_MEASUREMENT_ID : "G-NRB3DG38JH"
