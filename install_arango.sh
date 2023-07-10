cf825eed39f06d3c3e1010d6b102f079a6445b4e
Patrick Kirts <patkirts@gmail.com>
2023-07-10 11:37:49 -0400

    configurable ports

--- install_arango.sh
+++ install_arango.sh
@@ -5,20 +5,19 @@ export URLPREFIX=https://github.com/arangodb/kube-arangodb/releases/download/$VE
 
 # Check if namespace already exists
 if ! kubectl get namespace "$NAMESPACE" > /dev/null 2>&1; then
+  echo "Creating namespace $NAMESPACE"
   kubectl create namespace "$NAMESPACE"
 fi
 
 # Check if helm CRD chart is already installed
 if ! helm list -n "$NAMESPACE" | grep kube-arangodb-crd > /dev/null 2>&1; then
+  echo "Installing helm ArangoDB CRD chart"
   helm install kube-arangodb-crd "$URLPREFIX/kube-arangodb-crd-$VERSION.tgz" --namespace "$NAMESPACE"
 fi
 
-# Check if helm chart is already installed
-# if ! helm list -n "$NAMESPACE" | grep kube-arangodb > /dev/null 2>&1; then
-  helm install kube-arangodb "$URLPREFIX/kube-arangodb-$VERSION.tgz" --namespace "$NAMESPACE" --set "operator.features.storage=true" --set "operator.architectures={amd64,arm64}"
-# fi
+echo "Installing helm ArangoDB chart"
+helm install kube-arangodb "$URLPREFIX/kube-arangodb-$VERSION.tgz" --namespace "$NAMESPACE" --set "operator.features.storage=true" --set "operator.architectures={amd64,arm64}"
 
-# Check if ArangoDB Deployment already exists
-# if ! kubectl -n "$NAMESPACE" get ArangoDeployment arango-dev-cluster > /dev/null 2>&1; then
-  kubectl apply -f charts/arango-dev-cluster.yaml
-# fi
+echo "Creating ArangoDB Deployment"
+kubectl apply -f charts/arango-dev-cluster.yaml
+# kubectl apply -f charts/arango-dev-cluster-amd.yaml
