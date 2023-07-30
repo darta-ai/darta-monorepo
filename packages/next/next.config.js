acb35cdf07dd645855897211e5414e2659d6fdee
TJWetmore <tj.wetmore.github@gmail.com>
2023-07-30 12:33:11 -0400

    wip docker file for standalone

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -2,5 +2,13 @@ const withTM = require('next-transpile-modules')(['../components']);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withTM({});
+const path = require('path')
 
-module.exports = nextConfig;
+module.exports = {
+    ...nextConfig,
+    output: 'standalone',
+    experimental: {
+        // this includes files from the monorepo base two directories up
+        outputFileTracingRoot: path.join(__dirname, '../../'),
+      },
+};
