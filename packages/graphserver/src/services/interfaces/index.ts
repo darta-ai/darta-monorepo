37f9ccbbe38712d02a5268c611ffed138b13c99a
TJWetmore <tj.wetmore.github@gmail.com>
2023-09-01 16:21:29 -0400

    idk what's going on here

---
+++ packages/graphserver/src/services/interfaces/index.ts
@@ -0,0 +1,4 @@
+export {IArtworkService} from './IArtworkService'
+export {IGalleryService} from './IGalleryService'
+export {IImageService} from './IImageService'
+export {IAdminService} from './IAdminService'
