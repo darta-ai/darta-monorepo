f4744a5fd9c7164618b11c4314b1cb49d83e90b5
TJWetmore <tj.wetmore.github@gmail.com>
2023-10-25 13:00:00 -0400

    fix: removing unused files

--- packages/darta/src/components/Elements/GalleryIcon.tsx
+++ packages/darta/src/components/Elements/GalleryIcon.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {IconButton} from 'react-native-paper';
 
-import {IconT} from '../../typing/types';
 import { View, Image, StyleSheet } from 'react-native';
-import { PRIMARY_50 } from '@darta-styles';
 
 export function GalleryIcon({galleryLogo}: {galleryLogo: string}) {
 const galleryIconStyles = StyleSheet.create({
